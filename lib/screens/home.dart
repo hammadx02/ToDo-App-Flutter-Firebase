c24f3f1d0cf9410e09792ebfa96f115714615aa9
hammadx02 <hammadaali02@gmail.com>
2023-08-27 22:56:22 +0500

    updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/widgets/todo_items.dart';
 import '../constants/colors.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,234 +24,83 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Stack(
+      body: Column(
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            child: Column(
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(16),
+              physics: BouncingScrollPhysics(),
               children: [
-                const SizedBox(
-                  height: 30,
-                ),
                 searchBox(),
                 const SizedBox(
-                  height: 10,
+                  height: 20,
                 ),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 50,
-                          bottom: 20,
-                        ),
-                        child: const Text(
-                          'All ToDo\'s',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Expanded(
-                        child: FirebaseAnimatedList(
-                          query: ref,
-                          itemBuilder: (context, snapshot, animation, index) {
-                            final title =
-                                snapshot.child('title').value.toString();
-
-                            if (searchFilter.text.isEmpty) {
-                              return ListTile(
-                                leading: Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 8.0,
-                                    ),
-                                    child: ListTile(
-                                      onTap: () {
-                                        setState(() {
-                                          isDone = true;
-                                        });
-                                      },
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      tileColor: Colors.white,
-                                      leading: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            isDone = false;
-                                          });
-                                        },
-                                        child: Icon(
-                                          isDone
-                                              ? Icons.check_box
-                                              : Icons.check_box_outline_blank,
-                                          color: tdBlue,
-                                        ),
-                                      ),
-                                      title: Text(
-                                        title,
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: tdBlack,
-                                            decoration: isDone
-                                                ? TextDecoration.lineThrough
-                                                : TextDecoration.none),
-                                      ),
-                                      trailing: Container(
-                                        padding: EdgeInsets.all(0),
-                                        margin:
-                                            EdgeInsets.symmetric(vertical: 12),
-                                        height: 35,
-                                        width: 35,
-                                        decoration: BoxDecoration(
-                                          color: tdRed,
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                        ),
-                                        child: IconButton(
-                                          color: Colors.white,
-                                          iconSize: 18,
-                                          icon: Icon(Icons.delete),
-                                          onPressed: () {
-                                            ref
-                                                .child(snapshot
-                                                    .child('id')
-                                                    .value
-                                                    .toString())
-                                                .remove();
-                                          },
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else if (title.toLowerCase().contains(
-                                  searchFilter.text.toLowerCase().toString(),
-                                )) {
-                              return ListTile(
-                                leading: Container(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 8.0),
-                                    child: ListTile(
-                                      onTap: () {
-                                        setState(() {
-                                          isDone = true;
-                                        });
-                                      },
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                      tileColor: Colors.white,
-                                      leading: InkWell(
-                                        onTap: () {
-                                          setState(() {
-                                            isDone = false;
-                                          });
-                                        },
-                                        child: Icon(
-                                          isDone
-                                              ? Icons.check_box
-                                              : Icons.check_box_outline_blank,
-                                          color: tdBlue,
-                                        ),
-                                      ),
-                                      title: Text(
-                                        snapshot
-                                            .child('title')
-                                            .value
-                                            .toString(),
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: tdBlack,
-                                            decoration: isDone
-                                                ? TextDecoration.lineThrough
-                                                : TextDecoration.none),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            } else {
-                              return Container();
-                            }
-                          },
-                        ),
-                      ),
-                    ],
+                const Text(
+                  'All ToDo\'s',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          margin: const EdgeInsets.only(
-                            bottom: 20,
-                            right: 20,
-                            left: 20,
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 5,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: const [
-                              BoxShadow(
-                                color: Colors.grey,
-                                offset: Offset(0.0, 0.0),
-                                blurRadius: 10.0,
-                                spreadRadius: 0.0,
-                              ),
-                            ],
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: TextField(
-                            controller: todoController,
-                            decoration: const InputDecoration(
-                                hintText: 'Add a new todo item',
-                                border: InputBorder.none),
-                          ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TodoItems(),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
                         ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(
-                          bottom: 20,
-                          right: 20,
-                        ),
-                        child: ElevatedButton(
-                          // ignore: sort_child_properties_last
-                          child: const Text(
-                            '+',
-                            style: TextStyle(
-                              fontSize: 40,
-                            ),
-                          ),
-                          onPressed: () {
-                            String id = DateTime.now()
-                                .microsecondsSinceEpoch
-                                .toString();
-                            ref.child(id).set({
-                              'id': id,
-                              'title': todoController.text.toString()
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: tdBlue,
-                            minimumSize: const Size(60, 60),
-                            elevation: 10,
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      controller: todoController,
+                      decoration: const InputDecoration(
+                          hintText: 'Add a new todo item',
+                          border: InputBorder.none),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                ElevatedButton(
+                  // ignore: sort_child_properties_last
+                  child: const Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                  onPressed: () {
+                    String id =
+                        DateTime.now().microsecondsSinceEpoch.toString();
+                    ref.child(id).set(
+                        {'id': id, 'title': todoController.text.toString()});
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    minimumSize: const Size(60, 60),
+                    elevation: 10,
                   ),
                 ),
               ],
@@ -263,7 +113,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget searchBox() {
     return Container(
-      height: 55,
       padding: const EdgeInsets.symmetric(
         horizontal: 20,
       ),
--- lib/widgets/todo_items.dart
+++ lib/widgets/todo_items.dart
@@ -17,75 +17,66 @@ bool isDone = false;
 final searchFilter = TextEditingController();
 final todoController = TextEditingController();
 final auth = FirebaseAuth.instance;
-final ref = FirebaseDatabase.instance.ref('ToDos');
+final ref = FirebaseDatabase.instance.ref('ToDo\'s');
 
 class _TodoItemsState extends State<TodoItems> {
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
     return FirebaseAnimatedList(
+      shrinkWrap: true,
+      physics: BouncingScrollPhysics(),
       query: ref,
       itemBuilder: (context, snapshot, animation, index) {
         final title = snapshot.child('title').value.toString();
 
         if (searchFilter.text.isEmpty) {
           return ListTile(
-            leading: Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 8.0,
-                ),
-                child: ListTile(
-                  onTap: () {
-                    setState(() {
-                      isDone = true;
-                    });
-                  },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  tileColor: Colors.white,
-                  leading: InkWell(
-                    onTap: () {
-                      setState(() {
-                        isDone = false;
-                      });
-                    },
-                    child: Icon(
-                      isDone ? Icons.check_box : Icons.check_box_outline_blank,
-                      color: tdBlue,
-                    ),
-                  ),
-                  title: Text(
-                    title,
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: tdBlack,
-                        decoration: isDone
-                            ? TextDecoration.lineThrough
-                            : TextDecoration.none),
-                  ),
-                  trailing: Container(
-                    padding: const EdgeInsets.all(0),
-                    margin: const EdgeInsets.symmetric(vertical: 12),
-                    height: 35,
-                    width: 35,
-                    decoration: BoxDecoration(
-                      color: tdRed,
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: IconButton(
-                      color: Colors.white,
-                      iconSize: 18,
-                      icon: const Icon(Icons.delete),
-                      onPressed: () {
-                        ref
-                            .child(snapshot.child('id').value.toString())
-                            .remove();
-                      },
-                    ),
-                  ),
-                ),
+            onTap: () {
+              setState(() {
+                isDone = true;
+              });
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            tileColor: Colors.white,
+            leading: InkWell(
+              onTap: () {
+                setState(() {
+                  isDone = false;
+                });
+              },
+              child: Icon(
+                isDone ? Icons.check_box : Icons.check_box_outline_blank,
+                color: tdBlue,
+              ),
+            ),
+            title: Text(
+              title,
+              style: TextStyle(
+                  fontSize: 16,
+                  color: tdBlack,
+                  decoration: isDone
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
+            trailing: Container(
+              padding: const EdgeInsets.all(0),
+              margin: const EdgeInsets.symmetric(vertical: 12),
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                color: tdRed,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                color: Colors.white,
+                iconSize: 18,
+                icon: const Icon(Icons.delete),
+                onPressed: () {
+                  ref.child(snapshot.child('id').value.toString()).remove();
+                },
               ),
             ),
           );
@@ -93,41 +84,34 @@ class _TodoItemsState extends State<TodoItems> {
               searchFilter.text.toLowerCase().toString(),
             )) {
           return ListTile(
-            leading: Container(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ListTile(
-                  onTap: () {
-                    setState(() {
-                      isDone = true;
-                    });
-                  },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  tileColor: Colors.white,
-                  leading: InkWell(
-                    onTap: () {
-                      setState(() {
-                        isDone = false;
-                      });
-                    },
-                    child: Icon(
-                      isDone ? Icons.check_box : Icons.check_box_outline_blank,
-                      color: tdBlue,
-                    ),
-                  ),
-                  title: Text(
-                    snapshot.child('title').value.toString(),
-                    style: TextStyle(
-                        fontSize: 16,
-                        color: tdBlack,
-                        decoration: isDone
-                            ? TextDecoration.lineThrough
-                            : TextDecoration.none),
-                  ),
-                ),
+            onTap: () {
+              setState(() {
+                isDone = true;
+              });
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            tileColor: Colors.white,
+            leading: InkWell(
+              onTap: () {
+                setState(() {
+                  isDone = false;
+                });
+              },
+              child: Icon(
+                isDone ? Icons.check_box : Icons.check_box_outline_blank,
+                color: tdBlue,
               ),
+            ),
+            title: Text(
+              snapshot.child('title').value.toString(),
+              style: TextStyle(
+                  fontSize: 16,
+                  color: tdBlack,
+                  decoration: isDone
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
             ),
           );
         } else {
